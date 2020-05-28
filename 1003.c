99a4dee2ac940e899a3b5a9f599f3b8abc364056
leenayun <48146687+leenayun@users.noreply.github.com>
2020-05-28 17:45:48 +0900

    백준 1003
    백준 1003 C언어 (시간초과) >> 이유는 피보나치 수열을 쓰라는게 아니라 0,1 나오는 개수만 파악하면 되는데 문제를 잘 못 읽음 (수정해야지..)

---
+++ 1003.c
@@ -0,0 +1,35 @@
+int fibonacci(int n,int *ptr0,int *ptr1) {
+	if (n == 0) {
+		//printf("0");
+		(*ptr0)++;
+		return 0;
+	}
+	else if (n == 1) {
+		//printf("1");
+		(*ptr1)++;
+		return 1;
+	}
+	else {
+		return fibonacci(n-1,ptr0, ptr1) + fibonacci(n-2,ptr0, ptr1);
+	}
+}
+
+int main() {
+
+	int count;
+	int N;
+
+	scanf("%d",&count);
+	int count0;
+	int count1;
+
+	for (int i = 0;i < count;i++) {
+		count0 = 0;
+		count1 = 0;
+		scanf("%d", &N);
+		fibonacci(N, &count0, &count1);
+		printf("%d %d\n", count0, count1);
+	}
+	return 0;
+}
+
