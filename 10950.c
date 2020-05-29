195cb5fcf9e94f680a27c6044498dc4d0b1d1b06
leenayun <48146687+leenayun@users.noreply.github.com>
2020-05-29 17:46:11 +0900

    케이스만큼 덧셈하기

---
+++ 10950.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main() {
+	int T =0;    
+
+	scanf("%d", &T);
+	int A, B;
+	for (int i = 0;i < T;i++) {
+		scanf("%d %d", &A,&B);
+		printf("%d\n", A + B);
+	}
+	return 0;
+}
+
